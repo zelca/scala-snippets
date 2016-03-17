5c891d006301b70328b236eb2821b92ca6bea2dd
zelca <zelcadev@gmail.com>
2016-03-17 17:16:42 +0100

    Add partial function example

--- src/partialfunctions.scala
+++ src/partialfunctions.scala
@@ -1,28 +1,21 @@
 package object partialfunctions {
 
-  def fizz = new PartialFunction[Int, Int] {
+  def fizz = new PartialFunction[Int, Unit] {
     def apply(i: Int) = {
       print("Fizz")
-      i
     }
 
     def isDefinedAt(i: Int) = i % 3 == 0
   }
 
-  def buzz: PartialFunction[Int, Int] = {
-    case i if i % 5 == 0 =>
+  def buzz: PartialFunction[Int, Unit] = {
+    case i: Int if i % 5 == 0 =>
       print("Buzz")
-      i
   }
 
-  def other: PartialFunction[Int, Int] = {
-    case i =>
+  def other: PartialFunction[Int, Unit] = {
+    case i: Any =>
       print(i.toString)
-      i
-  }
-
-  def space: PartialFunction[Any, Any] = {
-    case _ => print(" ")
   }
 
 }
@@ -31,13 +24,15 @@ object partialFunctionsMain extends App {
 
   import scala.math.random
 
-  val data = 15 :: (1 to 100).map(i => (20 * random).toInt).toList
+  val data = 3 :: 15 :: (1 to 100).map(i => (20 * random).toInt).toList
 
   println(data)
 
   import partialfunctions._
 
-  val fizzBuzz = fizz andThen buzz  orElse other andThen space
+  println(buzz.isDefinedAt(6))
+
+  val fizzBuzz = fizz orElse buzz orElse other
 
   data.foreach(fizzBuzz)
 
